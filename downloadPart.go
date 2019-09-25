87cf7980b34b73791d9d8a87d7ef9206542d172c
lol3rrr <s.loler03@gmail.com>
2019-09-25 20:18:19 +0200

    Fixed another problem

--- downloadPart.go
+++ downloadPart.go
@@ -8,17 +8,19 @@ import (
 
 func downloadPart(url string) (string, error) {
   resp, err := http.Get(url)
-  if err != nil
+  if err != nil {
     return "", err
+  }
   defer resp.Body.Close()
 
   body, err := ioutil.ReadAll(resp.Body)
-  if err != nil
+  if err != nil {
     return "", err
+  }
 
   if resp.StatusCode != 200 {
     return "", errors.New("Could not download the Video-Part")
   }
 
-  return data, nil
+  return string(body), nil
 }
